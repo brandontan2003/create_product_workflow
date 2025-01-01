22278875812e1c4e961a8b6bd05d7c53424baf53
Brandon Tan <brandontan2003@gmail.com>
2025-01-01 16:06:25 +0800

    Initial Commit for Create Product Workflow

---
+++ src/main/java/com/example/create_product_workflow/CreateProductRequest.java
@@ -0,0 +1,10 @@
+package com.example.create_product_workflow;
+
+import java.math.BigDecimal;
+
+
+public class CreateProductRequest {
+    private String productName;
+    private String productDesc;
+    private BigDecimal price;
+}
