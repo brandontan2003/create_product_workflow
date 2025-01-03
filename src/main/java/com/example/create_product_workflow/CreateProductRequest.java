c124417f8dffe769f30b8a0ecc01ab38590d81d9
Brandon Tan <brandontan2003@gmail.com>
2025-01-04 00:09:08 +0800

    Update Workflow

--- src/main/java/com/example/create_product_workflow/CreateProductRequest.java
+++ src/main/java/com/example/create_product_workflow/CreateProductRequest.java
@@ -1,9 +1,9 @@
 package com.example.create_product_workflow;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 
-
-public class CreateProductRequest {
+public class CreateProductRequest implements Serializable {
     private String productName;
     private String productDesc;
     private BigDecimal price;
